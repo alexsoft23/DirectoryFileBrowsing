8a6daf1e1117f23ce6355cc0ccd407612a75d4ef
Alex Herasymchuk <alex.hera.101@gmail.com>
2016-08-28 18:49:05 +0300

    Add project files.

---
+++ DirectoryFileBrowsing/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace DirectoryFileBrowsing.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ DirectoryFileBrowsing/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace DirectoryFileBrowsing.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
